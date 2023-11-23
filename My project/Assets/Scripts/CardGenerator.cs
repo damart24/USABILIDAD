6f06a169641536d1b517aab4315e4060dda0a6c1
damart24 <82331644+damart24@users.noreply.github.com>
2023-11-23 16:32:38 +0100

    Se leen todas las cartas ya

--- My project/Assets/Scripts/CardGenerator.cs
+++ My project/Assets/Scripts/CardGenerator.cs
@@ -16,43 +16,67 @@ public class CardGenerator : MonoBehaviour
     private Sprite[] sprites_;
     List<Carta> cartas = new List<Carta>();
 
-    //private void Start()
-    //{
-    //    string filePath = Path.Combine("Assets", "CSV", fileName);
-    //    string[] lines = File.ReadAllLines(filePath);
+    private void Start()
+    {
+        string filePath = Path.Combine("Assets", "CSV", fileName);
+        string[] lines = File.ReadAllLines(filePath);
 
-    //    foreach (string line in lines)
-    //    {
-    //        string[] values = line.Split(',');
+        foreach (string line in lines)
+        {
+            string[] values = line.Split(',');
 
-    //        Carta carta = new Carta
-    //        {
-    //            Tema = values[0],
-    //            Nombre = values[1],
-    //            Personaje = values[2],
-    //            Pregunta = values[3],
-    //            Condicion = values[4],
-    //            Probabilidad = int.Parse(values[5]),
-    //            SobrescribirSi = values[6],
-    //            SiDinero = int.Parse(values[7]),
-    //            SiGente = int.Parse(values[8]),
-    //            SiFlora = int.Parse(values[9]),
-    //            SiFauna = int.Parse(values[10]),
-    //            SiAire = int.Parse(values[11]),
-    //            ExtrasSi = values[12],
-    //            SobrescribeNo = values[13],
-    //            NoDinero = int.Parse(values[14]),
-    //            NoGente = int.Parse(values[15]),
-    //            NoFlora = int.Parse(values[16]),
-    //            NoFauna = int.Parse(values[17]),
-    //            NoAire = int.Parse(values[18]),
-    //            ExtrasNo = values[19],
-    //            TextoExplicativo = values[20]
-    //        };
+            Carta carta = new Carta
+            {
+                Tema = values[0],
+                Nombre = values[1],
+                Personaje = values[2],
+                Pregunta = values[3],
+                Condicion = values[4],
+                // Utilizando int.TryParse para manejar la conversión a entero de Probabilidad
+                Probabilidad = int.TryParse(values[5], out int probabilidad) ? probabilidad : 0,
 
-    //        cartas.Add(carta);
-    //    }
-    //}
+                SobrescribirSi = values[6],
+
+                // Utilizando int.TryParse para manejar la conversión a entero de SiDinero
+                SiDinero = int.TryParse(values[7], out int siDinero) ? siDinero : 0,
+
+                // Utilizando int.TryParse para manejar la conversión a entero de SiGente
+                SiGente = int.TryParse(values[8], out int siGente) ? siGente : 0,
+
+                // Utilizando int.TryParse para manejar la conversión a entero de SiFlora
+                SiFlora = int.TryParse(values[9], out int siFlora) ? siFlora : 0,
+
+                // Utilizando int.TryParse para manejar la conversión a entero de SiFauna
+                SiFauna = int.TryParse(values[10], out int siFauna) ? siFauna : 0,
+
+                // Utilizando int.TryParse para manejar la conversión a entero de SiAire
+                SiAire = int.TryParse(values[11], out int siAire) ? siAire : 0,
+
+                ExtrasSi = values[12],
+                SobrescribeNo = values[13],
+
+                // Utilizando int.TryParse para manejar la conversión a entero de NoDinero
+                NoDinero = int.TryParse(values[14], out int noDinero) ? noDinero : 0,
+
+                // Utilizando int.TryParse para manejar la conversión a entero de NoGente
+                NoGente = int.TryParse(values[15], out int noGente) ? noGente : 0,
+
+                // Utilizando int.TryParse para manejar la conversión a entero de NoFlora
+                NoFlora = int.TryParse(values[16], out int noFlora) ? noFlora : 0,
+
+                // Utilizando int.TryParse para manejar la conversión a entero de NoFauna
+                NoFauna = int.TryParse(values[17], out int noFauna) ? noFauna : 0,
+
+                // Utilizando int.TryParse para manejar la conversión a entero de NoAire
+                NoAire = int.TryParse(values[18], out int noAire) ? noAire : 0,
+
+                ExtrasNo = values[19],
+                TextoExplicativo = values[20]
+            };
+            Debug.Log("Hola");
+            cartas.Add(carta);
+        }
+    }
 
     //Instancia la nueva carta que estará detrás del mazo
     //Le pone como primer hijo y cambia su sprite por un sprite aleatorio
