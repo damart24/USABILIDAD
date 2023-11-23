71863a40f67bff2d88ca84767760cb65a7f449c8
damart24 <82331644+damart24@users.noreply.github.com>
2023-11-23 15:53:25 +0100

    Por ahora va

--- My project/Assets/Scripts/CardGenerator.cs
+++ My project/Assets/Scripts/CardGenerator.cs
@@ -1,16 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CardGenerator : MonoBehaviour
 {
+    string fileName = "niveles-cartas.csv";
+    //string filePath = "niveles-cartas.csv";
     //Prefab de la carta que se va a generar
     [SerializeField]
     private GameObject cardPrefab_;
     //Lista de sprites aleatorios que se van a cambiar
     [SerializeField]
     private Sprite[] sprites_;
+    List<Carta> cartas = new List<Carta>();
+
+    //private void Start()
+    //{
+    //    string filePath = Path.Combine("Assets", "CSV", fileName);
+    //    string[] lines = File.ReadAllLines(filePath);
+
+    //    foreach (string line in lines)
+    //    {
+    //        string[] values = line.Split(',');
+
+    //        Carta carta = new Carta
+    //        {
+    //            Tema = values[0],
+    //            Nombre = values[1],
+    //            Personaje = values[2],
+    //            Pregunta = values[3],
+    //            Condicion = values[4],
+    //            Probabilidad = int.Parse(values[5]),
+    //            SobrescribirSi = values[6],
+    //            SiDinero = int.Parse(values[7]),
+    //            SiGente = int.Parse(values[8]),
+    //            SiFlora = int.Parse(values[9]),
+    //            SiFauna = int.Parse(values[10]),
+    //            SiAire = int.Parse(values[11]),
+    //            ExtrasSi = values[12],
+    //            SobrescribeNo = values[13],
+    //            NoDinero = int.Parse(values[14]),
+    //            NoGente = int.Parse(values[15]),
+    //            NoFlora = int.Parse(values[16]),
+    //            NoFauna = int.Parse(values[17]),
+    //            NoAire = int.Parse(values[18]),
+    //            ExtrasNo = values[19],
+    //            TextoExplicativo = values[20]
+    //        };
+
+    //        cartas.Add(carta);
+    //    }
+    //}
+
     //Instancia la nueva carta que estará detrás del mazo
     //Le pone como primer hijo y cambia su sprite por un sprite aleatorio
     public void InstantiateCard()
---
+++ My project/Assets/Scripts/Carta.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Carta : MonoBehaviour
+{
+    public string Tema;
+    public string Nombre;
+    public string Personaje;
+    public string Pregunta;
+    public string Condicion;
+    public int Probabilidad;
+    public string SobrescribirSi;
+    public int SiDinero;
+    public int SiGente;
+    public int SiFlora;
+    public int SiFauna;
+    public int SiAire;
+    public string ExtrasSi;
+    public string SobrescribeNo;
+    public int NoDinero;
+    public int NoGente;
+    public int NoFlora;
+    public int NoFauna;
+    public int NoAire;
+    public string ExtrasNo;
+    public string TextoExplicativo;
+}
+
+
