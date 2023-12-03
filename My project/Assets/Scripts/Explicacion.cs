9e22346a105cbaf6af64e03e32973156da48399b
jacall02 <82378764+jacall02@users.noreply.github.com>
2023-12-03 13:23:03 +0100

    assets explicaciones

---
+++ My project/Assets/Scripts/Explicacion.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Explicacion : MonoBehaviour
+{
+    public GameObject[] prefabs = new GameObject[5];
+    private void Start()
+    {
+    }
+}
